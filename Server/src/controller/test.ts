0e0326985efbd0544c92f08c49c4a6e2a9e5111a
olt13mm <olt13mm@gmail.com>
2022-04-26 23:51:09 +0900

    Chore: [Server] #44 sequelize 설치
    - sequelize config 구성
    - model 틀 작성

---
+++ Server/src/controller/test.ts
@@ -0,0 +1,14 @@
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize('database', 'username', 'password', {
+//   host: 'localhost',
+//   dialect: 'mysql',
+// // });
+
+// const sequelize = new Sequelize('sidePractice', 'root', 'yourPassword', {
+//   host: 'localhost',
+//   dialect: 'mysql',
+// });
+
+// const { Sequelize, Op, Model, DataTypes } = require('sequelize');
+// const sequelize = new Sequelize('sqlite::memory:');
